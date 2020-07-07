a5a98c597a53e21c21578c2d53fb83aa2dfdc7d6
Repeat Yang <repeat@pixnet.tw>
2020-07-07 11:24:54 +0800

    init

---
+++ bitrise-ios-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bitrise-ios-test
+//
+//  Created by repeat on 2020/7/7.
+//  Copyright © 2020 PIXNET Digital Media Corporation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
